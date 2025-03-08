7edaa5608ca7312ebba83a11adb4af3e75fd467b
champ-sk <shubhamkumar3183@gmail.com>
2025-03-08 16:59:47 +0530

    final commits

--- src/App.js
+++ src/App.js
@@ -38,39 +38,40 @@ function App() {
       <button onClick={clickHandler}>Open Form</button>
 
       {isOpen && (
-        <div className="modal modal-backdrop" onClick={closeHandler}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <form onSubmit={submitHandler}>
-              <h2>Fill Details</h2>
+  <div className="modal-backdrop" onClick={closeHandler}>
+    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+      <form onSubmit={submitHandler}>
+        <h2>Fill Details</h2>
 
-              <div className="input-group">
-                <label htmlFor="username">Username: </label>
-                <input type="text" name="username" id="username" required />
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="email">Email Address:</label>
-                <input type="email" name="email" id="email" required />
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="phoneNo">Phone Number:</label>
-                <input type="tel" name="phoneNo" id="phone" pattern="[0-9]{10}" required />
-              </div>
-
-              <div className="input-group">
-                <label htmlFor="dob">Date of Birth:</label>
-                <input type="date" name="dob" id="dob" required />
-              </div>
-
-              <button type="submit" className="submit-button">Submit</button>
-              <button type="button" className="close-button" onClick={() => setIsOpen(false)}>
-                Close
-              </button>
-            </form>
-          </div>
+        <div className="input-group">
+          <label htmlFor="username">Username: </label>
+          <input type="text" name="username" id="username" required />
         </div>
-      )}
+
+        <div className="input-group">
+          <label htmlFor="email">Email Address:</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="phoneNo">Phone Number:</label>
+          <input type="tel" name="phoneNo" id="phoneNo" pattern="[0-9]{10}" required />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="dob">Date of Birth:</label>
+          <input type="date" name="dob" id="dob" required />
+        </div>
+
+        <button type="submit" className="submit-button">Submit</button>
+        <button type="button" className="close-button" onClick={() => setIsOpen(false)}>
+          Close
+        </button>
+      </form>
+    </div>
+  </div>
+)}
+
     </div>
   );
 }
